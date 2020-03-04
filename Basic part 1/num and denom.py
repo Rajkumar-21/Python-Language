ff51008f9fcb09478d73dd7788b629b129a3b7bf
Rajkumar-21 <61075470+Rajkumar-21@users.noreply.github.com>
2020-03-04 11:07:20 +0530

    Add files via upload

---
+++ Basic part 1/num and denom.py
@@ -0,0 +1,5 @@
+# Develop a Python Program to read numerator and denominator and print their quotient and remainder.
+a=int(input("Enter Numerator :\n"))
+b=int(input("Enter Denomintor :\n"))
+print("Quotient of the the Values : \t {}".format(a/b))
+print("Remainder of the the Values : \t {}".format(a%b))
---
+++ Basic part 1/swap.py
@@ -0,0 +1,35 @@
+# Swap Two numbers using three variables
+print("Swap Two numbers using three variables")
+
+a=10
+b=20
+c=0
+print("Before swapping")
+print("Value of a is {}" .format(a))
+print("Value of b is {}" .format(b))
+c=b
+b=a
+a=c
+print("After swapping")
+print("Value of a is {}" .format(a))
+print("Value of b is {}" .format(b))
+
+
+# Swap Two numbers using three variables
+
+print("Swap Two numbers using Two variables")
+
+
+a=10
+b=20
+
+print("Before swapping")
+print("Value of a is {}" .format(a))
+print("Value of b is {}" .format(b))
+
+a=b
+b=a
+
+print("After swapping")
+print("Value of a is {}" .format(a))
+print("Value of b is {}" .format(b))
